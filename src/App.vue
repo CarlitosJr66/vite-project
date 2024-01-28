9ca7ec0e9138583999b25ff00d4ad978b657730b
CarlitosJr66 <carloshahajaja@gmail.com>
2024-01-28 10:54:03 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue'
+const name = ref('') 
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Advanced Web Design" />
-</template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="name" v-model="name"  />
+      </h2>
+    </section>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <section class="create-todo">
+
+    </section>
+
+    <section class="todo-list">
+
+    </section>
+  </main>
+</template>
